ff6cd802f2af9f5c2a035ac6a412f4f69d29a078
stankovics <stankovicnstefan@gmail.com>
2022-02-24 20:41:19 -0600

    adding js operations

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,14 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
   }
+  clear() {}
+  delete() {}
+  appendNumber(number) {}
+  chooseOperation(operation) {}
+  compute() {}
+  updateDisplay() {}
 }
 const calculator = new Calculator(
   previousOperandTextElement,
