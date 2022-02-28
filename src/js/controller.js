a367bde97d4908a20979bec6ff1f33f092707c6d
stankovics <stankovicnstefan@gmail.com>
2022-02-27 20:22:41 -0600

    mcv update

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,10 @@
 import * as model from './model';
+import { calculator } from './view';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 const init = function () {};
 init();
+
+model.chooseOperation();
+model.compute();
